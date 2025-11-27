97f268c6ef2fd8b9c2268ec179238781c85e6957
adeliabraguta <adeliabraguta@gmail.com>
2025-11-27 20:40:04 +0200

    Rename component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./components/home-page/home-page').then((m) => m.HomePage),
-    title: 'Home',
+    loadComponent: () => import('./components/module-1/module-1').then((m) => m.Module1),
+    title: 'Module1',
   },
 ];
